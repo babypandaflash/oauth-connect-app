d6efaa2e3408a32f0235ce9c17bb679d6fb8f459
babypandaflash <babypandaflash@gmail.com>
2025-06-20 19:20:22 +0700

    Delete api/auth/youtube.js

--- api/auth/youtube.js
+++
@@ -1,6 +0,0 @@
-const passport = require("../../../passport");
-const app = require("../../../server");
-
-app.get("/api/auth/youtube", passport.authenticate("google", { scope: ["profile", "email"] }));
-
-module.exports = app;
